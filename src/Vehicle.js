3f5410a6d61450b40204daa65650339c1214c5f7
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-27 17:20:03 -0700

    refactored code in trafficlight.js, vehicle.js, and vehiclelist.js

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,17 +1,17 @@
-export default function Vehicle(props) {
+export default function Vehicle({ vehicle }) {
   return <div className="vehicle">
 
     {/* the vehicle component has a prop called vehicle. Check to see what string is in this vehicle prop */}
     {/* if the vehicle props is a car, render a '🚗' emoji */}
-    {props.vehicle === 'car' && '🚗'}
+    {vehicle === 'car' && '🚗'}
 
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
-    {props.vehicle === 'truck' && '🚚'}
+    {vehicle === 'truck' && '🚚'}
 
     {/* if the vehicle props is a bus, render a '🚌' emoji */}
-    {props.vehicle === 'bus' && '🚌'}
+    {vehicle === 'bus' && '🚌'}
 
     {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
-    {props.vehicle === 'motorcycle' && '🏍️'}
+    {vehicle === 'motorcycle' && '🏍️'}
   </div>;
 }
--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Vehicle from './Vehicle.js';
 
-export default function VehicleList(props) {
+export default function VehicleList({ vehicles }) {
   return (
     <div className="traffic">
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
       {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
-      {props.vehicles.map(vehicle => <Vehicle vehicles={vehicle} key={vehicle} vehicle={vehicle} />)}
+      {vehicles.map((vehicle, index) => <Vehicle vehicles={vehicle} key={vehicle + index} vehicle={vehicle} />)}
     </div>
   );
 }
